ede930225a5d539d7de00c197c703efa28054ed8
ajit-gvs <77957533+ajit-gvs@users.noreply.github.com>
2022-08-10 21:11:01 +0530

    used pandas to display a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text("idli and sambhar")
 streamlit.text("bread and butter")
 streamlit.text("🤞dosa and chutney😏")
 streamlit.header("Make your own smoothie")
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
