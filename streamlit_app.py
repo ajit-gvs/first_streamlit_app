80f451fcc5308f3cf3ab7f08ed3e5c33d9447900
ajit-gvs <77957533+ajit-gvs@users.noreply.github.com>
2022-08-10 21:08:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
 streamlit.title("My parents new healthy diner")
-streamlit.header("\N{flexed biceps}Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("idli and sambhar")
 streamlit.text("bread and butter")
-streamlit.text("\N{vulcan salute}dosa and chutney")
+streamlit.text("🤞dosa and chutney😏")
 streamlit.header("Make your own smoothie")
