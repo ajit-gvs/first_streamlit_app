e9bf4d9b646c776d044bdc1e8e37fc4ccc63dcd9
ajit-gvs <77957533+ajit-gvs@users.noreply.github.com>
2022-08-10 21:13:13 +0530

    created a user interactive widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,7 @@ streamlit.text("🤞dosa and chutney😏")
 streamlit.header("Make your own smoothie")
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
